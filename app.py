ea81845733db64ed82ef4f93418b3ece19dfb56b
Ubuntu <ubuntu@ip-172-31-45-158.ap-south-1.compute.internal>
2024-07-02 20:47:14 +0000

    changes in app.py

--- app.py
+++ app.py
@@ -24,4 +24,4 @@ def index():
     return render_template('add.html', result=result)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8000)
+    app.run(debug=True, host='0.0.0.0', port=8000)
